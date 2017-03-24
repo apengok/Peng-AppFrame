343ed3caa29b4880966de333e5ab3cd7a8da7feb
apengok <apengok@163.com>
2017-03-24 17:39:01 +0800

    add manager.py

---
+++ manager.py
@@ -0,0 +1,14 @@
+from flask_script import Manager
+from flask_script.commands import Server,Shell,ShowUrls,Clean
+
+from flask_application import app
+
+
+manager = Manager(app)
+
+manager.add_command("shell",Shell(use_ipython=True))
+manager.add_command("show_urls",ShowUrls())
+
+
+if __name__ == '__main__':
+    manager.run()
